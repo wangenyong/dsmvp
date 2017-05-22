4266f5ea079390ac42cd859e4b884278a880efd5
wangenyong <wangenyong@gmail.com>
2017-05-22 14:28:15 +0800

    first commit

---
+++ mvp/src/main/java/com/wangenyong/mvp/bind/BindClick.java
@@ -0,0 +1,22 @@
+package com.wangenyong.mvp.bind;
+
+/**
+ * Created by wangenyong on 2017/5/22.
+ */
+
+import android.support.annotation.IdRes;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * 在ContentView内绑定点击事件
+ */
+@Retention(RUNTIME)
+@Target({METHOD})
+public @interface BindClick {
+    @IdRes int value();
+}
