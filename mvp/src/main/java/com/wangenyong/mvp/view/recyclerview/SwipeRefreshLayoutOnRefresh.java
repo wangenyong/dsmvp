c897a03a9aae2c9855f8fac7acca370d36c8cadf
wangenyong <wangenyong@gmail.com>
2017-05-23 14:20:57 +0800

    Add custom recyclerview

---
+++ mvp/src/main/java/com/wangenyong/mvp/view/recyclerview/SwipeRefreshLayoutOnRefresh.java
@@ -0,0 +1,23 @@
+package com.wangenyong.mvp.view.recyclerview;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+
+/**
+ * Created by wangenyong on 2017/5/23.
+ */
+
+public class SwipeRefreshLayoutOnRefresh implements SwipeRefreshLayout.OnRefreshListener {
+    private PullLoadMoreRecyclerView mPullLoadMoreRecyclerView;
+
+    public SwipeRefreshLayoutOnRefresh(PullLoadMoreRecyclerView pullLoadMoreRecyclerView) {
+        this.mPullLoadMoreRecyclerView = pullLoadMoreRecyclerView;
+    }
+
+    @Override
+    public void onRefresh() {
+        if (!mPullLoadMoreRecyclerView.isRefresh()) {
+            mPullLoadMoreRecyclerView.setIsRefresh(true);
+            mPullLoadMoreRecyclerView.refresh();
+        }
+    }
+}
