aea45ca0b05b87f932f11bdc22d0dc85047dd55a
wangenyong <wangenyong@gmail.com>
2017-05-25 11:05:05 +0800

    init hawk

--- mvp/src/main/java/com/wangenyong/mvp/base/BaseApplication.java
+++ mvp/src/main/java/com/wangenyong/mvp/base/BaseApplication.java
@@ -2,6 +2,8 @@ package com.wangenyong.mvp.base;
 
 import android.app.Application;
 
+import com.orhanobut.hawk.Hawk;
+
 /**
  * Created by wangenyong on 2017/5/24.
  */
@@ -10,5 +12,6 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Hawk.init(this).build();
     }
 }
