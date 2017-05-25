9644c272ab647785c59b57936f7daf2ca4281f08
wangenyong <wangenyong@gmail.com>
2017-05-25 13:58:52 +0800

    Add bottom bar

--- demo/src/main/java/com/wangenyong/dsmvp/MainView.java
+++ demo/src/main/java/com/wangenyong/dsmvp/MainView.java
@@ -1,5 +1,12 @@
 package com.wangenyong.dsmvp;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.wangenyong.mvp.bind.BindId;
 import com.wangenyong.mvp.bind.BindLayout;
 import com.wangenyong.mvp.view.ContentView;
 
@@ -9,6 +16,22 @@ import com.wangenyong.mvp.view.ContentView;
 
 @BindLayout(R.layout.activity_main)
 public class MainView extends ContentView {
+    @BindId(R.id.bottom_bar)
+    BottomNavigationViewEx mNavigationViewEx;
 
+    public View createView(Context context, @Nullable Bundle savedInstanceState) {
+        View view = super.createView(context, savedInstanceState);
 
+        mNavigationViewEx.enableAnimation(false);
+        mNavigationViewEx.enableShiftingMode(false);
+        mNavigationViewEx.enableItemShiftingMode(false);
+        mNavigationViewEx.setTextVisibility(false);
+        // set icon size
+        int iconSize = 32;
+        mNavigationViewEx.setIconSize(iconSize, iconSize);
+        // set item height
+        mNavigationViewEx.setItemHeight(BottomNavigationViewEx.dp2px(context, iconSize + 24));
+        mNavigationViewEx.setIconsMarginTop(BottomNavigationViewEx.dp2px(context, 12));
+        return view;
+    }
 }
