7ca9edcfd0d6cb4892f662efb66e5e4133282004
wangenyong <wangenyong@gmail.com>
2017-05-25 15:53:22 +0800

    Add retrofit2

---
+++ demo/src/main/java/com/wangenyong/dsmvp/http/ApiService.java
@@ -0,0 +1,8 @@
+package com.wangenyong.dsmvp.http;
+
+/**
+ * Created by wangenyong on 2017/5/25.
+ */
+
+public class ApiService {
+}
---
+++ demo/src/main/java/com/wangenyong/dsmvp/http/Url.java
@@ -0,0 +1,9 @@
+package com.wangenyong.dsmvp.http;
+
+/**
+ * Created by wangenyong on 2017/5/25.
+ */
+
+public class Url {
+    public static final String BASE_URL = "https://api.douban.com/v2/movie/";
+}
