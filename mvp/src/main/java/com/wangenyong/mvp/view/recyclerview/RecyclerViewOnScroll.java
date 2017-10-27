856ef1f4be269f4a5d55704ff179f697fcc1a83b
wangenyong <wangenyong@gmail.com>
2017-10-27 17:03:27 +0800

    优化RecyclerViewOnScroll类.

--- mvp/src/main/java/com/wangenyong/mvp/view/recyclerview/RecyclerViewOnScroll.java
+++ mvp/src/main/java/com/wangenyong/mvp/view/recyclerview/RecyclerViewOnScroll.java
@@ -6,7 +6,9 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 
 /**
- * Created by wangenyong on 2017/5/23.
+ *
+ * @author wangenyong
+ * @date 2017/5/23
  */
 
 public class RecyclerViewOnScroll extends RecyclerView.OnScrollListener {
@@ -28,12 +30,16 @@ public class RecyclerViewOnScroll extends RecyclerView.OnScrollListener {
             firstItem = gridLayoutManager.findFirstCompletelyVisibleItemPosition();
             //Position to find the final item of the current LayoutManager
             lastItem = gridLayoutManager.findLastCompletelyVisibleItemPosition();
-            if (lastItem == -1) lastItem = gridLayoutManager.findLastVisibleItemPosition();
+            if (lastItem == -1) {
+                lastItem = gridLayoutManager.findLastVisibleItemPosition();
+            }
         } else if (layoutManager instanceof LinearLayoutManager) {
             LinearLayoutManager linearLayoutManager = ((LinearLayoutManager) layoutManager);
             firstItem = linearLayoutManager.findFirstCompletelyVisibleItemPosition();
             lastItem = linearLayoutManager.findLastCompletelyVisibleItemPosition();
-            if (lastItem == -1) lastItem = linearLayoutManager.findLastVisibleItemPosition();
+            if (lastItem == -1) {
+                lastItem = linearLayoutManager.findLastVisibleItemPosition();
+            }
         } else if (layoutManager instanceof StaggeredGridLayoutManager) {
             StaggeredGridLayoutManager staggeredGridLayoutManager = ((StaggeredGridLayoutManager) layoutManager);
             // since may lead to the final item has more than one StaggeredGridLayoutManager the particularity of the so here that is an array
@@ -44,8 +50,9 @@ public class RecyclerViewOnScroll extends RecyclerView.OnScrollListener {
             firstItem = staggeredGridLayoutManager.findFirstVisibleItemPositions(lastPositions)[0];
         }
         if (firstItem == 0 || firstItem == RecyclerView.NO_POSITION) {
-            if (mPullLoadMoreRecyclerView.getPullRefreshEnable())
+            if (mPullLoadMoreRecyclerView.getPullRefreshEnable()) {
                 mPullLoadMoreRecyclerView.setSwipeRefreshEnable(true);
+            }
         } else {
             mPullLoadMoreRecyclerView.setSwipeRefreshEnable(false);
         }
@@ -61,7 +68,9 @@ public class RecyclerViewOnScroll extends RecyclerView.OnScrollListener {
 
     }
 
-    //To find the maximum value in the array
+    /**
+     * To find the maximum value in the array
+     */
     private int findMax(int[] lastPositions) {
         int max = lastPositions[0];
         for (int value : lastPositions) {
