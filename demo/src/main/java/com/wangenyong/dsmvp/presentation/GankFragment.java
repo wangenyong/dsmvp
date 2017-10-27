b98c4a10282080b59e9e7f0d6016b05bfc2492e7
wangenyong <wangenyong@gmail.com>
2017-10-27 16:19:17 +0800

    删除GankFragmentView

--- demo/src/main/java/com/wangenyong/dsmvp/presentation/GankFragment.java
+++ demo/src/main/java/com/wangenyong/dsmvp/presentation/GankFragment.java
@@ -1,6 +1,7 @@
 package com.wangenyong.dsmvp.presentation;
 
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -8,22 +9,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.wangenyong.dsmvp.R;
+import com.wangenyong.dsmvp.databinding.FragmentGankBinding;
 import com.wangenyong.dsmvp.entity.Gank;
 import com.wangenyong.dsmvp.http.Api;
-import com.wangenyong.dsmvp.view.GankFragmentView;
+import com.wangenyong.dsmvp.itembinder.GankViewBinder;
 import com.wangenyong.mvp.base.BaseFragment;
 import com.wangenyong.mvp.http.HttpUtils;
 import com.wangenyong.mvp.http.ProgressObserver;
+import com.wangenyong.mvp.util.UiUtils;
+import com.wangenyong.mvp.view.recyclerview.PullLoadMoreRecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
+import me.drakeet.multitype.MultiTypeAdapter;
 
-public class GankFragment extends BaseFragment implements GankFragmentView.ActionImpl {
-    private GankFragmentView contentView = new GankFragmentView();
-    private List<Gank> mGanks;
+public class GankFragment extends BaseFragment implements PullLoadMoreRecyclerView.PullLoadMoreListener {
+    private List<Gank> mGanks = new ArrayList<>();
     private int mPage = 1;
+
+    private FragmentGankBinding mBinding;
+    private MultiTypeAdapter mAdapter;
 
     public static GankFragment newInstance() {
         GankFragment fragment = new GankFragment();
@@ -31,18 +39,25 @@ public class GankFragment extends BaseFragment implements GankFragmentView.Actio
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAdapter = new MultiTypeAdapter();
+        mAdapter.register(Gank.class, new GankViewBinder());
+        mAdapter.setItems(mGanks);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = contentView.createView(inflater, savedInstanceState);
-        contentView.setActionImpl(this);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_gank, container, false);
+        View view = mBinding.getRoot();
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mGanks = new ArrayList<>();
-        contentView.initView(mGanks);
+        initRecyclerView();
         getGank(mPage, false, false);
     }
 
@@ -50,6 +65,12 @@ public class GankFragment extends BaseFragment implements GankFragmentView.Actio
     public void onRefresh() {
         mPage = 1;
         getGank(mPage, true, false);
+    }
+
+    private void initRecyclerView() {
+        mBinding.recyclerViewGank.setLinearLayout();
+        mBinding.recyclerViewGank.setAdapter(mAdapter);
+        mBinding.recyclerViewGank.setOnPullLoadMoreListener(this);
     }
 
     @Override
@@ -62,13 +83,12 @@ public class GankFragment extends BaseFragment implements GankFragmentView.Actio
         HttpUtils.getInstance().toSubscribe(ob, new ProgressObserver<List<Gank>>(getActivity(), isRefresh, isLoadMore, " ") {
             @Override
             protected void _onNext(List<Gank> ganks) {
-                mGanks.addAll(ganks);
-                if (isLoadMore) {
-                    contentView.loadMoreSuccess();
-                } else {
+                if (isRefresh) {
                     ganks.clear();
-                    contentView.showGans();
                 }
+                mGanks.addAll(ganks);
+                mBinding.recyclerViewGank.setPullLoadMoreCompleted();
+                mAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -77,7 +97,8 @@ public class GankFragment extends BaseFragment implements GankFragmentView.Actio
                 if (isLoadMore) {
                     mPage--;
                 }
-                contentView.onError(getActivity(), message);
+                mBinding.recyclerViewGank.setPullLoadMoreCompleted();
+                UiUtils.makeText(getActivity(), message);
             }
         }, "cacheKey", this, false, true);
     }
